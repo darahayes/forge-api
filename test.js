bccac0d705ccd7ef3a85c1b28d45ff14ab350acb
Darragh Hayes <dara.hayes.1995@gmail.com>
2016-01-19 22:54:52 +0000

    version 1 of exercises api

--- test.js
+++ test.js
@@ -1,19 +1,28 @@
 var request = require('request');
-request = request.defaults({jar: true});
+request = request.defaults({jar: true, headers: {'Content-Type': 'application/json'}});
 var login = {};
 
-request.post({url: 'http://localhost:3000/auth/login', form: {email: 'dara@example.com', password: 'password'}}, function(err,response,body) {
-    console.log(err);
-    console.log(response.headers['content-type']);
-    // console.log(response)
-    login = JSON.parse(body).login;
-    console.log(response.state);
-    console.log(login);
-    request.get({url: 'http://localhost:3000/api/hello', token: login}, function(err, resp, body) {
-    console.log(err);
-    // console.log(resp)
-    console.log(JSON.parse(body));
+request({method: 'POST', url: 'http://localhost:3000/auth/login', json: true, body: {email: 'dara@example.com', password: 'password'}}, function(err,response,body) {
+  console.log(err);
+  console.log(response.headers['content-type']);
+  // console.log(response)
+  login = body.login;
+  console.log(response.state);
+  console.log(login);
+  request({method: 'POST', url:'http://localhost:3000/api/exercises', json: true, body: {"name": "my_pushup", "category": "Resistance", "tags": ["Chest", "Arms"]}}, function(err,response,body){ 
+      console.log(response.headers['content-type']);
+      console.log(err)
+      console.log(body)
   });
+  request({method: 'GET', url:'http://localhost:3000/api/exercises'}, function(err,resp,body) {
+    console.log('body', body);
+  })
+  // request({method: 'GET', url:'http://localhost:3000/api/exercises', qs: {userExercises: true}}, function(err,resp,body) {
+  //   console.log('body', body);
+  // })
+  // request({method: 'DELETE', url:'http://localhost:3000/api/exercises'}, function(err,resp,body) {
+  //   console.log(err, body);
+  // })
 });
 
 // request.get({url: 'http://localhost:3000/api/hello', token: login}, function(err, resp, body) {
