76863dbc45e985f91c8472a90f91e6580cd1895a
Darragh Hayes <dara.hayes.1995@gmail.com>
2016-01-19 13:29:29 +0000

    created a default authentication strategy

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
 var Chairo = require('chairo');
 var Hapi = require('hapi');
 var options = require('./config');
-var Hapi_Cookie = require('hapi-auth-cookie'); 
+var Hapi_Cookie = require('hapi-auth-cookie');
+var boom = require('boom');
 
 var server = new Hapi.Server();
 server.connection({ port: 3000 });
-server.method('getUser', getUser, {}); 
 // Register plugin
+
+server.auth.scheme('custom', scheme);
+server.auth.strategy('logged_in', 'custom');
+// server.auth.default('default');
 
 server.route({
   method: 'GET',
   path: '/api/hello',
-  handler: function (request, reply) {
-    server.methods.getUser(request, function(err, out) {
-      return reply.act({role: 'calendar', cmd: 'history', user: out.user.id});
-    });
+  config: {
+    auth: 'logged_in',
+    handler: function (request, reply) {
+      return reply.act({role: 'calendar', cmd: 'history', user: request.auth.credentials.login});
+    }
   }
 });
 
@@ -47,24 +52,29 @@ server.register(plugins, function (err) {
   });
 });
 
+function scheme (server, options) {
+  return {
+    authenticate: function (request, reply) {
+      if (request.state['seneca-login']) {
+        var token = request.state['seneca-login']['seneca-login'];
+        request.seneca.act({role: 'user', cmd:'auth', token: token}, function (err, resp) {
+          if (err) return reply(err);
+          if (resp.ok === false) {
+            return reply('login not ok');
+          }
+          return reply.continue({ credentials: { login: resp.login } });
+        });
+      }
+      else {
+        return reply(boom.unauthorized('You must be logged in to continue'));
+      }
+    }
+  };
+}
+
 function checkHapiPluginError(error) {
   if (error) {
     console.log('An error occurred while loading a hapu plugin');
     throw error;
-  }
-}
-
-function getUser (request, cb) {
-  var token = request.state['seneca-login']['seneca-login'];
-  if (token) {
-    request.seneca.act({role: 'user', cmd:'auth', token: token}, function (err, resp) {
-      if (err) return cb(err);
-      if (resp.ok === false) {
-        return cb('login not ok');
-      }
-      return cb(null, resp);
-    });
-  } else {
-    setImmediate(cb);
   }
 }
