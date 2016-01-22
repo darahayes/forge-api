89cb11dfedb727604ed124b34021be4f4e8fcafe
Darragh Hayes <dara.hayes.1995@gmail.com>
2016-01-22 12:29:30 +0000

    bump version number

--- test.js
+++ test.js
@@ -2,18 +2,29 @@ var request = require('request');
 request = request.defaults({jar: true, headers: {'Content-Type': 'application/json'}});
 var login = {};
 
-request({method: 'POST', url: 'http://localhost:3000/auth/login', json: true, body: {email: 'dara@example.com', password: 'password'}}, function(err,response,body) {
+var host = '46.101.92.165';
+var port = 3000;
+var url = `http://${host}:${port}`;
+
+// request({method: 'POST', url: `${url}/auth/register`, json: true, body: {email: 'dara@example.com', password: 'password'}}, function(err,response,body) {
+//   console.log(err);
+//   console.log(response.headers['content-type']);
+//   console.log(body);
+  
+// });
+
+request({method: 'POST', url: `${url}/auth/login`, json: true, body: {email: 'dara@example.com', password: 'password'}}, function(err,response,body) {
   console.log(err);
   console.log(response.headers['content-type']);
   // console.log(response)
   login = body.login;
   console.log(response.state);
   console.log(login);
-  // request({method: 'POST', url:'http://localhost:3000/api/exercises', json: true, body: {"name": "my_pushup", "category": "Resistance", "tags": ["Chest", "Arms"]}}, function(err,response,body){ 
-  //     console.log(response.headers['content-type']);
-  //     console.log(err)
-  //     console.log(body)
-  // });
+  request({method: 'POST', url:`${url}/api/exercises`, json: true, body: {"name": "my_pushup", "category": "Resistance", "tags": ["Chest", "Arms"]}}, function(err,response,body){ 
+      console.log(response.headers['content-type']);
+      console.log(err)
+      console.log(body)
+  });
   // request({method: 'GET', url:'http://localhost:3000/api/exercises'}, function(err,resp,body) {
   //   console.log('body', body);
   // })
@@ -23,18 +34,18 @@ request({method: 'POST', url: 'http://localhost:3000/auth/login', json: true, bo
   // request({method: 'DELETE', url:'http://localhost:3000/api/exercises'}, function(err,resp,body) {
   //   console.log(err, body);
   // })
-  var e = {name: 'dips', category : 'Resistance', id: 'dips_id', sets: [{weight: 15, reps: 10, unit :'lbs'}, {weight: 10, reps: 12, unit :'lbs'}]}
-  var e1 = {name: 'dips1', category : 'Resistance', id: 'dips_id', sets: [{weight: 15, reps: 10, unit :'lbs'}, {weight: 10, reps: 12, unit :'lbs'}]}
-  var e2 = {name: 'dips2', category : 'Resistance', id: 'dips_id', sets: [{weight: 15, reps: 10, unit :'lbs'}, {weight: 10, reps: 12, unit :'lbs'}]}
-  var e3 = {name: 'dips3', category : 'Resistance', id: 'dips_id', sets: [{weight: 15, reps: 10, unit :'lbs'}, {weight: 10, reps: 12, unit :'lbs'}]}
-  var e4 = {name: 'running', category: 'Cardio', id: 'running_id', distance: 6.4, unit: 'km', time_ms: 1234567890987654321}
-  var body = { exercises: [e, e1, e2, e3, e4]}
+  // var e = {name: 'dips', category : 'Resistance', id: 'dips_id', sets: [{weight: 15, reps: 10, unit :'lbs'}, {weight: 10, reps: 12, unit :'lbs'}]}
+  // var e1 = {name: 'dips1', category : 'Resistance', id: 'dips_id', sets: [{weight: 15, reps: 10, unit :'lbs'}, {weight: 10, reps: 12, unit :'lbs'}]}
+  // var e2 = {name: 'dips2', category : 'Resistance', id: 'dips_id', sets: [{weight: 15, reps: 10, unit :'lbs'}, {weight: 10, reps: 12, unit :'lbs'}]}
+  // var e3 = {name: 'dips3', category : 'Resistance', id: 'dips_id', sets: [{weight: 15, reps: 10, unit :'lbs'}, {weight: 10, reps: 12, unit :'lbs'}]}
+  // var e4 = {name: 'running', category: 'Cardio', id: 'running_id', distance: 6.4, unit: 'km', time_ms: 1234567890987654321}
+  // var body = { exercises: [e, e1, e2, e3, e4]}
 
-  request({method: 'POST', url:'http://localhost:3000/api/calendar', json: true, body: body}, function(err,response,body){ 
-      console.log(response.headers['content-type']);
-      console.log(err)
-      console.log(body)
-  });
+  // request({method: 'POST', url:'http://localhost:3000/api/calendar', json: true, body: body}, function(err,response,body){ 
+  //     console.log(response.headers['content-type']);
+  //     console.log(err)
+  //     console.log(body)
+  // });
 });
 
 // request.get({url: 'http://localhost:3000/api/hello', token: login}, function(err, resp, body) {
